5d23fca6aeccbb5c80aef831a7cf398ff1a7cf8b
Sanjay_S_ecec <107357668+SanjaySashikumar@users.noreply.github.com>
2023-03-23 19:26:22 +0530

    Update main.js

--- GAYNMAT_PRG_2/main.js
+++ GAYNMAT_PRG_2/main.js
@@ -4,8 +4,18 @@ let cont=document.getElementById('to_do');
 
 b.addEventListener('click', function(){
 	//create a para tag in the to_do ID
-	var text = document.createElement('p')
+	var text = document.createElement('p');
+	//styling the added list elements
+	text.classList.add('para-styling');
 	text.innerText = chores.value;
 	//add the input text into the pata tag
 	cont.appendChild(text);
+	inputtext.value="";
+		text.addEventListener('click',function(){
+		text.style.textDecoration = "line-through";
+		
+		text.addEventListener('click',function(){
+			cont.removeChild(text);
+		}) 
+		})
 })
